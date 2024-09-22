1b47826c4c36f101253f7b89ec147589e81f92f1
ibrahim <ebrahimaboeita990@gmail.com>
2024-09-22 13:48:48 +0300

    Renaming some directories

--- server/middlewares/logger.js
+++
@@ -1,8 +0,0 @@
-// backend/middleware/logger.js
-
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
-
-module.exports = { logger };
--- server/middlewares/notFound.js
+++
@@ -1,9 +0,0 @@
-// backend/middleware/notFound.js
-
-const notFound = (req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-};
-
-module.exports = { notFound };
