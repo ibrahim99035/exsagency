bd6cd6cf72a09eb88676ea0722c2c06db7f4aefe
ibrahim <ebrahimaboeita990@gmail.com>
2024-12-09 15:47:40 +0200

    Color and Navbar Adjustments

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -3,9 +3,14 @@ import '../../CSS/Header.css';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [language, setLanguage] = useState('EN'); // Initial language state
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
+  };
+
+  const toggleLanguage = () => {
+    setLanguage((prevLang) => (prevLang === 'EN' ? 'AR' : 'EN')); // Toggle between EN and AR
   };
 
   return (
@@ -15,16 +20,18 @@ const Header = () => {
       </div>
       <nav className={`nav ${menuOpen ? 'open' : ''}`}>
         <ul>
-          
           <li><a href="#blog">Blog</a></li>
           <li><a href="/contact-us">Contact</a></li>
-          <li><a href="/gallery">Gallery</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="/services">Services</a></li>
           <li><a href="/about-us">About</a></li>
+          <li><a href="/gallery">Gallery</a></li>
+          <li><a href="/services">Services</a></li>
           <li><a href="/">Home</a></li>
         </ul>
       </nav>
+      <div className="language-toggle" onClick={toggleLanguage}>
+        {language}
+      </div>
       <div className="menu-toggle" onClick={toggleMenu}>
         <span className={`bar ${menuOpen ? 'open' : ''}`}></span>
         <span className={`bar ${menuOpen ? 'open' : ''}`}></span>
